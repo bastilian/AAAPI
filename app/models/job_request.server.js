fb5d1413d6f5e7da03a587e41cc86eb9d1f6cac6
Sebastian Gräßl <bastilian@users.noreply.github.com>
2023-06-13 01:15:31 +0200

    feat: Add Job model (#11)

---
+++ app/models/job_request.server.js
@@ -0,0 +1,20 @@
+import { prisma } from '~/app/db.server';
+
+export const getId = async (id) =>
+  (
+    await prisma.jobRequest.findMany({
+      where: {
+        id,
+      },
+    })
+  )[0];
+
+export async function create(data) {
+  return prisma.jobRequest.create({
+    data,
+  });
+}
+
+export async function deleteById(id) {
+  return prisma.jobRequest.delete({ where: { id } });
+}
