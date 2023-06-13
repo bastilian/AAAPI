4545c681c8f05241a3831eb4c89073837a551e2c
Aleksandr Voznesenskii <avoznese@redhat.com>
2023-06-13 22:48:10 +0200

    update job and tests

--- app/routes/async/job.jsx
+++ app/routes/async/job.jsx
@@ -1,18 +1,16 @@
-import { create } from '../../models/job_request.server'
-import { prisma } from '../../db.server'
+
+import * as job from '../../models/job.server'
+import * as jobReq from '../../models/job_request.server'
 
 export const loader = async ({request: { body } }) => {
-  console.log(body, 'request')
-  const data = body
-  const newJob = await prisma.job.create()
-  for (const newJobRequest of new data) {
-    await create({data: {
+  const newJob = await job.create();
+  for (const newJobRequest of body.urls) {
+    await jobReq.create({data: {
         jobId: newJob.id,
         url: newJobRequest.url,
-        params: newJobRequest.params
       }})
   }
-  return prisma.job.findFirst({ where: { id: newJob.id }, include: { jobRequests: true } })
+  return job.findFirst({ where: { id: newJob.id }, include: { jobRequests: true } })
 };
 
 export const action = loader;
--- app/routes/async/job.test.js
+++ app/routes/async/job.test.js
@@ -3,17 +3,19 @@ import { prisma } from '../../db.server'
 
 describe('loader test', () => {
     const body = {
-        "urls": [
-          {"url": "http://example.org/items", "params": { "testParam": "testParamValue", "page": 1 }},
-          {"url": "http://example.org/items", "params": { "testParam": "testParamValue", "page": 2 }}
+        urls: [
+          {url: "http://example.org/items", params: { testParam: "testParamValue", page: 1 }},
+          {url: "http://example.org/items", params: { testParam: "testParamValue", page: 2 }}
         ]
       }
     test('compare the job requests length', async () => {
-        const jobsAmount = await prisma.job.findMany()
+        const jobsReqAmount = await prisma.jobRequest.findMany({ where: { jobId: { not: undefined}}})
+        console.log(jobsReqAmount.length)
         loader({request: {
             body
           }});
-        const newJobsAmount = await prisma.job.findMany()
-        expect(newJobsAmount.length).toEqual(jobsAmount.length + 1)
+        const newJobsReqAmount = await prisma.jobRequest.findMany({ where: { jobId: { not: undefined}}})
+        console.log(newJobsReqAmount.length)
+        expect(newJobsReqAmount.length).toEqual(jobsReqAmount.length)
     })
 })
