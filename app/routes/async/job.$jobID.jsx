ba59acd1e95afcda9626e78b50cf3878df9e7f66
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2023-06-15 10:18:31 -0400

    feat(Issue:6): test for checking status of jobs (#24)
    * feat(Issue:6): test for checking status of jobs
    
    * Addressed comments
    
    * file name change
    
    * change switch case

---
+++ app/routes/async/job.$jobID.jsx
@@ -0,0 +1,19 @@
+import { json } from "@remix-run/node";
+import { getById } from "../../models/job.server";
+
+export const loader = async ({ params: { jobId }})=>  {
+  //function displays different statuses based of api response
+  const foundJob = await getById(jobId)
+  const renderSwitch = () => {
+    switch (foundJob.status) {
+      case "new":
+        return "new status";
+      case "done":
+        return "done status";
+      case "pending":
+        return "pending status"
+    }
+  };
+  let results =  json({ "statusKey": renderSwitch()})
+  return results
+}
---
+++ app/routes/async/job.jobID.test.jsx
@@ -0,0 +1,40 @@
+import { describe, expect, test } from 'vitest'
+
+import { create, updateJobById } from '../../models/job.server';
+import {loader} from './job.$jobID'
+ 
+describe('Returns proper status based off jobs current attribute', () => {
+  
+  test('should return status of new', async () => {
+    const newJob = await create();
+    const doneJob = await updateJobById(newJob.id, { status: 'new'});
+    const response = await loader({ params: { jobId: doneJob.id }});
+
+    expect(response.status).toBe(200)
+    expect( await response.json()).toEqual({
+      statusKey: 'new status'
+    })
+  })
+  test('should return status of pending', async () => {
+    const newJob = await create();
+    const doneJob = await updateJobById(newJob.id, { status: 'pending'});
+    const response = await loader({ params: { jobId: doneJob.id }});
+
+    expect(response.status).toBe(200)
+    expect( await response.json()).toEqual({
+      statusKey: 'pending status'
+    })
+  })
+ 
+  test('should return status of done', async () => {
+    const newJob = await create();
+    const doneJob = await updateJobById(newJob.id, { status: 'done'});
+    const response = await loader({ params: { jobId: doneJob.id }});
+
+    expect(response.status).toBe(200)
+    expect( await response.json()).toEqual({
+      statusKey: 'done status'
+    })
+  })
+ 
+})
